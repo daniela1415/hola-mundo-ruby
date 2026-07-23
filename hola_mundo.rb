180064964a7b6d5464d4b8fe30303820ebb1b1ed
daniela1415 <dandreacastro@ucundinamarca.edu.co>
2026-07-22 19:43:23 -0500

    Agrega calculo de edad para 2030

--- hola_mundo.rb
+++ hola_mundo.rb
@@ -1,4 +1,11 @@
 puts "¿Cuál es tu nombre?"
 nombre = gets.chomp
 
-puts "Hola #{nombre}, bienvenida a Ruby"
+puts "¿Cuál es tu edad?"
+edad = gets.chomp.to_i
+
+puts "Hola #{nombre}, tienes #{edad} años, bienvenida a Ruby"
+
+edad_2030 = edad + 4
+
+puts "En 2030 tendrás #{edad_2030} años."
